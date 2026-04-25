afdfb87379d632b1224a47b26b4ee46d1f816321
name <name@SERGEYOSIN>
2026-04-25 22:37:14 +0300

    решение проблем с коммитом

--- FoodSelectionApi.Test/obj/Debug/net10.0/FoodSelectionApi.Test.AssemblyInfo.cs
+++ FoodSelectionApi.Test/obj/Debug/net10.0/FoodSelectionApi.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodSelectionApi.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe83bacba334365fdec06b2817e504e3c45069e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9adc11f3f35d91f3f040f45b83fb3b81d829260")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodSelectionApi.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodSelectionApi.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
